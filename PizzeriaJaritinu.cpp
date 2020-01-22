6a750c66f914be2b8553148df962cdccc4cd6d53
jaritinu <oscar.arp@hotmail.com>
2020-01-22 06:45:59 -0600

    Modify: menu

--- PizzeriaJaritinu.cpp
+++ PizzeriaJaritinu.cpp
@@ -39,56 +39,99 @@ restaurantOrder* restaurantList;
 int restaurantOrderNumber = 0;
 int deliveryOrderNumber = 0;
 
-void addDeliveryOrder(), addRestaurantOrder(), showDeliveryOrder(),showRestaurantOrder(), searchOrderbyClient();
+void addDeliveryOrder(), addRestaurantOrder(), showDeliveryOrder(),showRestaurantOrder(), searchOrderbyClient(), delteOrder();
 
 int main(){
     // Mostrar menu al usuario
     string clave = "vector";
     string password = "";
     int counterPass = 0;
+    int user = 0;
     bool flagPass = false;
     cout << "------Pizzeria Jaritinu------" <<endl;
-    do{
-        cout << "Digite el password: ";
-        getline(cin,password);
-        if (password == clave){
-            flagPass = true;
+    cout << "1. Empleado";
+    cout << "2. Administrador";
+    cout << "Seleccione el tipo de usuario (1 o 2): ";
+
+    switch (user)
+    {
+    case 1:
+        do{
+            cout << "Digite el password: ";
+            getline(cin,password);
+            if (password == clave){
+                flagPass = true;
+            }
+            else{
+                cout << "\nPassword incorrecto." << endl;
+                counterPass++;
+            }
+        }while (flagPass == false && counterPass < 3);
+    
+        if (flagPass==true){
+            bool continuar = true;
+            do{
+                int opcion = 0;
+                cout << "\n1. Agregar pedido a domicilio\n";
+                cout << "2. Agregar encargo en restaurante\n";
+                cout << "3. Ver pedidos a domicilio\n";
+                cout << "4. Buscar orden por cliente\n";
+                cout << "5. Eliminar orden\n";
+                cout << "6. Ver encargas a restaurantes\n";
+                cout << "7. Ver total de venta\n";
+                cout << "8. Salir\n";
+                cout << "Opcion: ";
+                cin >> opcion;
+                cin.ignore();
+                
+                switch(opcion){
+                    case 1: addDeliveryOrder(); break;
+                    case 2: addRestaurantOrder(); break;
+                    case 3: showDeliveryOrder(); break;
+                    case 4: searchOrderbyClient();break;
+                    case 5: delteOrder();break;
+                    case 6: showRestaurantOrder();break;
+                    case 7: totalSells();break;
+                    case 8: continuar = false;
+                }
+            }while(continuar);
         }
         else{
-            cout << "\nPassword incorrecto." << endl;
-            counterPass++;
+            cout << "\nFallo los 3 intentos" << endl;
         }
-    }while (flagPass == false && counterPass < 3);
-    
-    if (flagPass==true){
+        break;
+    case 2:
         bool continuar = true;
         do{
-            int opcion = 0;
-            cout << "\n1. Agregar pedido a domicilio\n";
-            cout << "2. Agregar encargo en restaurante\n";
-            cout << "3. Ver pedidos a domicilio\n";
-            cout << "4. Buscar orden por cliente\n";
-            cout << "5. Ver encargas a restaurantes\n";
-            cout << "6. Ver total de venta\n";
-            cout << "7. Salir\n";
-            cout << "Opcion: ";
-            cin >> opcion;
-            cin.ignore();
-            
-            switch(opcion){
-                case 1: addDeliveryOrder(); break;
-                case 2: addRestaurantOrder(); break;
-                case 3: showDeliveryOrder(); break;
-                case 4: searchOrderbyClient();break;
-                case 5: showRestaurantOrder();break;
-                case 6: totalSells();break;
-                case 7: continuar = false;
-            }
-        }while(continuar);
+                int opcion = 0;
+                cout << "\n1. Agregar pedido a domicilio\n";
+                cout << "2. Agregar encargo en restaurante\n";
+                cout << "3. Ver pedidos a domicilio\n";
+                cout << "4. Buscar orden por cliente\n";
+                cout << "5. Ver encargas a restaurantes\n";
+                cout << "6. Ver total de venta\n";
+                cout << "7. Salir\n";
+                cout << "Opcion: ";
+                cin >> opcion;
+                cin.ignore();
+                
+                switch(opcion){
+                    case 1: addDeliveryOrder(); break;
+                    case 2: addRestaurantOrder(); break;
+                    case 3: showDeliveryOrder(); break;
+                    case 4: searchOrderbyClient();break;
+                    case 5: showRestaurantOrder();break;
+                    case 6: totalSells();break;
+                    case 7: continuar = false;
+                }
+            }while(continuar);
+    default:
+        return 0;
+        break;
     }
-    else{
-        cout << "\nFallo los 3 intentos" << endl;
-    }
+
+
+    
     return 0;
 }
 void addDeliveryOrder(){
@@ -98,6 +141,10 @@ void addRestaurantOrder(){
 
 } 
 void showDeliveryOrder(){
+
+}
+
+void delteOrder(){
 
 }
 
